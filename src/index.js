13ba3ad6f127f6b3feba17c2d8e467fcf3ace2d5
Shevchenko Andrey <demorest49de@gmail.com>
2023-10-27 13:38:39 +0300

    Changes car.js changed; gasstation.js changed; index.js changed;

--- src/index.js
+++ src/index.js
@@ -1,45 +1,58 @@
 import './style.css';
+import {Car, PassengerCar, Truck} from "./modules/car.js";
+import {Station} from "./modules/gasstation";
 
 const open = document.querySelector('.open');
 const car = document.querySelector('.car');
 
 const testArray = {
-  passangerCar: [
-    ['Opel', 'Crossland', 45],
-    ['Opel', 'Grandland X', 53],
-    ['Mazda', 'cx-5', 55],
-    ['BMW', 'M5', 68],
-    ['BMW', 'X5', 80],
-    ['BMW', 'X5d', 80, 'diesel'],
-    ['BMW', 'X3', 65],
-    ['BMW', '5', 66],
-  ],
-  truck: [
-    ['MAN', 'TGS', 400],
-    ['MAN', 'TGX', 300],
-    ['Mercedes-Benz', 'Actros', 450],
-    ['Mercedes-Benz', 'Actros L', 650],
-    ['Volvo', 'FH16', 700],
-    ['Volvo', 'FM', 700],
-    ['Volvo', 'FMX', 540],
-  ],
+    passangerCar: [
+        ['Opel', 'Crossland', 45],
+        ['Opel', 'Grandland X', 53],
+        ['Mazda', 'cx-5', 55],
+        ['BMW', 'M5', 68],
+        ['BMW', 'X5', 80],
+        ['BMW', 'X5d', 80, 'diesel'],
+        ['BMW', 'X3', 65],
+        ['BMW', '5', 66],
+    ],
+    truck: [
+        ['MAN', 'TGS', 400],
+        ['MAN', 'TGX', 300],
+        ['Mercedes-Benz', 'Actros', 450],
+        ['Mercedes-Benz', 'Actros L', 650],
+        ['Volvo', 'FH16', 700],
+        ['Volvo', 'FM', 700],
+        ['Volvo', 'FMX', 540],
+    ],
 };
 
 const getTestCar = () => {
-  const typeBool = Math.random() < 0.6;
-  const listCar = typeBool ? testArray.passangerCar : testArray.truck;
-  const randomCar = listCar[(Math.floor(Math.random() * listCar.length))];
-  return randomCar;
+    const typeBool = Math.random() < 0.6;
+    const listCar = typeBool ? testArray.passangerCar : testArray.truck;
+    const randomCar = listCar[(Math.floor(Math.random() * listCar.length))];
+    return typeBool ? new PassengerCar(...randomCar) : new Truck(...randomCar);
 };
 
 
+const station = new Station([
+    {
+        type: 'petrol',
+        count: 2,
+        speed: 5,
+    },
+    {
+        type: 'diesel',
+        count: 1,
+        speed: 10,
+    }
+]);
+
 open.addEventListener('click', () => {
-  console.log('Открыто');
-  open.remove();
-  car.style.display = 'block';
-  car.addEventListener('click', () => {
-    console.log(getTestCar());
-  });
+    console.log('Открыто');
+    open.remove();
+    car.style.display = 'block';
+    car.addEventListener('click', () => {
+        station.addCarQueue(getTestCar());
+    });
 });
-
-
---
+++ src/modules/gasstation.js
@@ -0,0 +1,13 @@
+export class Station {
+    constructor(type) {
+        this.type = type;
+        this.queue = [];
+        this.filling = [];
+        this.ready = [];
+    }
+    
+    //todo инициализация должна быть
+    init() {
+    
+    }
+}
